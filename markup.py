b3cf7f93564645a997ca68b272bb5a7791788f1c
BlindFury6501 <100614441+BlindFury6501@users.noreply.github.com>
2022-03-02 21:57:11 +0300

    Update markup.py

--- markup.py
+++ markup.py
@@ -1,14 +1,24 @@
+from telnetlib import KERMIT
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 btnMain = KeyboardButton('Меню')
 
 # --- main menu ---
 btnNews = KeyboardButton('Хочешь узнать что делает бот?')
-btnMarsh = KeyboardButton('Маршруты')
+btnMarsh = KeyboardButton('Достопримечательности')
 mainMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnNews, btnMarsh, )
 
 
 #--- Marsh menu ---
 
-btnPatrik = KeyboardButton('Патрики,Кремль,ЦУМ')
-MarshMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnPatrik, btnMain)
+btnPatrik = KeyboardButton('Патриаршие пруды')
+btnBteatr = KeyboardButton('Большой театр')
+btnNewarbat = KeyboardButton('Новый Арбат')
+btnkreml = KeyboardButton('Кремль')
+btnSobVas = KeyboardButton('Собор Василия Блаженного')
+btnHramkrest = KeyboardButton('Храм Христа Спасителя')
+btnmgu = KeyboardButton('МГУ')
+btnvdnx = KeyboardButton('ВДНХ')
+btnpark = KeyboardButton('Парк Победы')
+btnostank = KeyboardButton('Останкинская телебашня')
+MarshMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnPatrik, btnBteatr, btnkreml, btnSobVas, btnHramkrest, btnmgu, btnvdnx, btnpark, btnostank, btnMain)
